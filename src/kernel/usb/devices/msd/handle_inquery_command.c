93a2e0cc9064ba7b828503892c4f39d9b9742d6f
Alexandros de Regt <alexandrosderegtvisvliet@gmail.com>
2026-03-21 21:23:07 +0100

    usbstick

--- src/kernel/usb/devices/msd/handle_inquery_command.c
+++ src/kernel/usb/devices/msd/handle_inquery_command.c
@@ -2,15 +2,15 @@
 
 void handle_inquery_command(XHCIControllerSession *session, USBDevice* device, TransferTRB* transfer_event){
     inquiry_response* inc = (inquiry_response*) (uint64_t)transfer_event->DataBufferPointerLo;
-    printk("USB STICK vendor_id ");
+    printk("USB STICK ");
     for(int i = 0 ; i < 8 ; i++){
         printk("%c",inc->vendor_id[i]);
     }
-    printk(" product_id ");
+    printk(" ");
     for(int i = 0 ; i < 16 ; i++){
         printk("%c",inc->product_id[i]);
     }
-    printk(" product_revision ");
+    printk(" ");
     for(int i = 0 ; i < 4 ; i++){
         printk("%c",inc->product_revision[i]);
     }
